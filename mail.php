f08884bbf684afede560ea92ce24d8a33ff23981
sarbsK <168292909+sarbsK@users.noreply.github.com>
2024-04-28 19:27:32 +0530

    Delete mail.php

--- mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$recipient = "sarbeswar.crazylionstudio@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
-?>
